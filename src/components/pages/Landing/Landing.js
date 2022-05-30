53c4afa27debf4ba084e849e161a2e0f2e99c952
sarahemaginnis <sarahemaginnis@gmail.com>
2022-05-30 14:38:12 -0500

    landing page layout

--- src/components/pages/Landing/Landing.js
+++ src/components/pages/Landing/Landing.js
@@ -1,5 +1,35 @@
-const Landing = () => {
-    return <h1>The Landing Page</h1>
-}
+import React from "react";
+import landingCoffee from "./landingCoffee.png";
+import { Container, Row, Col } from "react-bootstrap";
+import "./Landing.css";
 
-export default Landing
+const Landing = () => {
+  return (
+    <div>
+      <div className="header">
+        <h1>LegalEase</h1>
+      </div>
+      <Container>
+        <Row>
+          <Col xs={6}>
+            <h2>Organize your study of the law</h2>
+            <p>
+              The best way to save, annotate, and organize your case briefs and
+              class notes for your law school outlines.
+            </p>
+            <button>Login</button>
+          </Col>
+          <Col xs={6}>
+            <img
+              className="image__landing"
+              src={landingCoffee}
+              alt="a steaming cup of coffee"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Landing;
