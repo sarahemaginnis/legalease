57a7ddfb8656280ec0a57b303578cbf80f495af2
sarahemaginnis <sarahemaginnis@gmail.com>
2022-05-30 12:46:20 -0500

    created atomic design folder structure with components

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import Account from "./components/pages/Account";
-import Brief from "./components/pages/Brief";
-import Contact from "./components/pages/Contact";
-import Dashboard from "./components/pages/Dashboard";
-import Landing from "./components/pages/Landing";
-import Login from "./components/pages/Login";
-import SignUp from "./components/pages/SignUp";
+import Account from "./components/pages/Account/Account";
+import Brief from "./components/pages/Brief/Brief";
+import Contact from "./components/pages/Contact/Contact";
+import Dashboard from "./components/pages/Dashboard/Dashboard";
+import Landing from "./components/pages/Landing/Landing";
+import Login from "./components/pages/Login/Login";
+import SignUp from "./components/pages/SignUp/SignUp";
 
 const ApplicationViews = () => {
   return (
