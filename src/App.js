d6b058ac28e54869b6d33fdd82803341a465e4fd
sarahemaginnis <sarahemaginnis@gmail.com>
2022-05-31 18:39:41 -0500

    footer navigation bar layout and links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import ApplicationViews from "./ApplicationViews";
+import Footer from "./components/organisms/footer/Footer";
 
 const App = () => {
   return (
     <div className="App">
       <h2>Let's get started!</h2>
       <ApplicationViews />
+      <Footer />
     </div>
   );
 };
