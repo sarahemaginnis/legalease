5a7417163465f5453cb16ef2ccd4ccd1b7ba71a6
sarahemaginnis <sarahemaginnis@gmail.com>
2022-05-31 21:29:50 -0500

    login page, conditional rendering of website to check for registered user, sign up page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from "react";
+import { Route, Redirect } from "react-router-dom";
 import "./App.css";
 import ApplicationViews from "./ApplicationViews";
 import Footer from "./components/organisms/footer/Footer";
+import Landing from "./components/pages/landing/Landing";
+import Login from "./components/pages/login/Login";
+import SignUp from "./components/pages/signUp/SignUp";
 
-const App = () => {
-  return (
-    <div className="App">
-      <ApplicationViews />
-      <Footer />
-    </div>
-  );
-};
+const App = () => (
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("legalease_user")) {
+          return (
+            <>
+              <ApplicationViews />
+              <Footer />
+            </>
+          );
+        } else {
+          return <Redirect to="/landing" />;
+        }
+      }}
+    />
+
+    <Route path="/landing">
+      <Landing />
+    </Route>
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/signup">
+      <SignUp />
+    </Route>
+  </>
+)
 
 export default App;
