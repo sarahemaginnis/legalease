fbab3073342e4aee98878dbc26e5c0d1caca129a
sarahemaginnis <sarahemaginnis@gmail.com>
2022-06-11 10:13:59 -0500

    contact form; bug fix on logout button

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,5 +1,87 @@
-const Contact = () => {
-    return <h1>The Contact Page</h1>
-}
+import "./Contact.css";
+import { Container, Row, Col } from "react-bootstrap";
+import contact from "./contact.png";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import InputText from "../../atoms/inputTypeComponent/InputText";
+import TextArea from "../../atoms/inputTypeComponent/TextArea";
 
-export default Contact
+const Contact = () => {
+  const [name, updateName] = useState("");
+  const [email, updateEmail] = useState("");
+  const [message, updateMessage] = useState("");
+
+  const history = useHistory();
+
+  const saveMessage = (event) => {
+    event.preventDefault();
+    const newMessage = {
+      creatorId: parseInt(localStorage.getItem("legalease_user")),
+      name: name,
+      email: email,
+      message: message,
+    };
+
+    const fetchOption = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newMessage),
+    };
+
+    return fetch("http://localhost:8088/messages", fetchOption).then(() => {
+      history.push("/dashboard");
+    });
+  };
+
+  return (
+    <>
+      <div className="card__contact">
+        <Container>
+          <Row>
+            <Col sm={4}>
+              <main className="contact">
+                <section>
+                  <form className="form__contact">
+                    <h1 className="h3 mb-3 font-weight-normal">Contact</h1>
+                    <p>We love hearing from you.</p>
+                    <p>
+                      Fill out this form and we'll be sure to get back to you
+                      right away.
+                    </p>
+                    <InputText
+                      label={"Your Name"}
+                      onChange={updateName}
+                      placeholder={""}
+                    />
+                    <InputText
+                      label={"Email"}
+                      onChange={updateEmail}
+                      placeholder={""}
+                    />
+                    <TextArea label={"Message"} onChange={updateMessage} />
+                    <fieldset>
+                      <button
+                        type="submit"
+                        className="btn__btn-primary"
+                        onClick={saveMessage}
+                      >
+                        Send Your Note
+                      </button>
+                    </fieldset>
+                  </form>
+                </section>
+              </main>
+            </Col>
+            <Col sm={8}>
+              <img className="image__contact" src={contact} alt="an envelope" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
