aa6f03ade8f9c11da9efb50d1e8a59fd4ffb0e97
sarahemaginnis <sarahemaginnis@gmail.com>
2022-06-18 12:06:33 -0500

    modal for delete brief confirmation; when user clicks on the delete brief icon, a modal message pops up, verifying if the user truly wants to delete the brief

---
+++ src/components/atoms/modal/ModalDeleteBrief.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
+import "./ModalDeleteBrief.css";
+
+const ModalDeleteBrief = ({ brief, deleteFunction, ...props }) => {
+  const deleteBrief = () => {
+    deleteFunction(brief.id)
+  };
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      centered
+      className="modal__delete"
+    >
+      <Modal.Header className="modal__header" closeButton></Modal.Header>
+      <Modal.Body className="modal__body">
+        <h4>Delete {brief ? brief.name : ""}?</h4>
+        <p>Are you sure you want to delete this brief?</p>
+      </Modal.Body>
+      <Modal.Footer className="modal__footer">
+        <button className="btn__btn-secondary" onClick={props.onHide}>Cancel</button>
+        <button className="btn__btn-primary" onClick={deleteBrief}>Delete</button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ModalDeleteBrief;
