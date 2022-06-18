0cedf2a81550976426b45e59c5d57fc05d29511e
sarahemaginnis <sarahemaginnis@gmail.com>
2022-06-18 13:11:21 -0500

    updated some css; incorporated more modal messages with user login and signup page

--- src/components/atoms/modal/ModalDeleteBrief.js
+++ src/components/atoms/modal/ModalDeleteBrief.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import "./ModalDeleteBrief.css";
 
 const ModalDeleteBrief = ({ brief, deleteFunction, ...props }) => {
